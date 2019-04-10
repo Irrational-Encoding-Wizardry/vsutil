4bbe21c537321faab0ba4b6242eb1578e35c66c9
Roland Netzsch <stuxcrystal@encode.moe>
2019-04-10 15:21:17 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
 from setuptools import setup
+
+
+def discover_tests():
+    import unittest
+    path = os.path.join(os.path.dirname(__file__), "tests")
+    return unittest.TestLoader().discover(path, pattern="test_*.py")
+
 
 setup(
     name='vsutil',
@@ -11,5 +19,6 @@ setup(
     description='A collection of general-purpose Vapoursynth functions to be reused in modules and scripts.',
     install_requires=[
         "vapoursynth"
-    ]
+    ],
+    test_suite="setup.discover_tests"
 )
