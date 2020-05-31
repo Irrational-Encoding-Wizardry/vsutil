32d5d34529904e0fcadcda4761fed45825dbe889
Roland Netzsch <stuxcrystal@users.noreply.github.com>
2020-05-31 15:22:34 +0200

    Manually import the exported functions

---
+++ vsutil/__init__.py
@@ -0,0 +1,13 @@
+"""
+VSUtil. A collection of general-purpose VapourSynth functions to be reused in modules and scripts.
+"""
+
+from vsutil.vsutil import Dither, Range,
+from vsutil.vsutil import depth, fallback, frame2clip
+from vsutil.vsutil import get_depth, get_plane_size
+from vsutil.vsutil import get_subsampling, get_w, get_y
+from vsutil.vsutil import insert_clip, is_image, iterate
+from vsutil.vsutil import join, plane, split
+
+__all__ = ['Dither', 'Range', 'depth', 'fallback', 'frame2clip', 'get_depth', 'get_plane_size',
+           'get_subsampling', 'get_w', 'get_y', 'insert_clip', 'is_image', 'iterate', 'join', 'plane', 'split']
