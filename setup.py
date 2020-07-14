5ff158e43cd245ceeebcce4583f9a037f5870645
kageru <kageru@encode.moe>
2020-07-14 09:59:18 +0200

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class DiscoverTest(test):
 
 setup(
     name='vsutil',
-    version='0.4.0',
+    version='0.5.0',
     packages=find_packages(exclude=['tests']),
     package_data={
         'vsutil': ['py.typed']
