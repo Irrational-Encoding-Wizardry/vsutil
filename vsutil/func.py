7f6044a469a729af257102f8473311529a202a94
Dave <orangechannel@pm.me>
2020-07-26 16:21:48 -0400

    Update docstrings for func.py

--- vsutil/func.py
+++ vsutil/func.py
@@ -1,5 +1,5 @@
 """
-_
+Non-vapoursynth related functions and decorators.
 """
 __all__ = [
     # decorators
@@ -19,9 +19,9 @@ R = TypeVar('R')
 
 
 def disallow_variable_format(function: F) -> F:
-    """
-    Function decorator that raises an exception if the input clip has a variable format.
-    Decorated function's first parameter must be of type `vapoursynth.VideoNode` and is the only parameter checked.
+    """Function decorator that raises an exception if the input clip has a variable format.
+
+    Decorated `function`'s first parameter must be of type ``vapoursynth.VideoNode`` and is the only parameter checked.
     """
 
     @functools.wraps(function)
@@ -34,9 +34,9 @@ def disallow_variable_format(function: F) -> F:
 
 
 def disallow_variable_resolution(function: F) -> F:
-    """
-    Function decorator that raises an exception if the input clip has a variable resolution.
-    Decorated function's first parameter must be of type `vapoursynth.VideoNode` and is the only parameter checked.
+    """Function decorator that raises an exception if the input clip has a variable resolution.
+
+    Decorated `function`'s first parameter must be of type ``vapoursynth.VideoNode`` and is the only parameter checked.
     """
 
     @functools.wraps(function)
@@ -49,15 +49,35 @@ def disallow_variable_resolution(function: F) -> F:
 
 
 def fallback(value: Optional[T], fallback_value: T) -> T:
-    """
-    Utility function that returns a value or a fallback if the value is None.
+    """Utility function that returns a value or a fallback if the value is ``None``.
+
+    >>> fallback(5, 6)
+    5
+    >>> fallback(None, 6)
+    6
+
+    :param value:           Argument that can be ``None``.
+    :param fallback_value:  Fallback value that is returned if `value` is ``None``.
+
+    :return:                The input `value` or `fallback_value` if `value` is ``None``.
     """
     return fallback_value if value is None else value
 
 
 def iterate(base: T, function: Callable[[Union[T, R]], R], count: int) -> Union[T, R]:
-    """
-    Utility function that executes a given function a given number of times.
+    """Utility function that executes a given function a given number of times.
+
+    >>> def double(x):
+    ...     return x * 2
+    ...
+    >>> iterate(5, double, 2)
+    20
+
+    :param base:      Initial value.
+    :param function:  Function to execute.
+    :param count:     Number of times to execute `function`.
+
+    :return:          `function`'s output after repeating `count` number of times.
     """
     if count < 0:
         raise ValueError('Count cannot be negative.')
