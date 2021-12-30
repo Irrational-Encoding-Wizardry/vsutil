6d1d416290739314efeb537c649d75728566b6c2
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2021-12-30 01:48:15 +0100

    Disallow variable formats, fix integer neutral value

--- vsutil/info.py
+++ vsutil/info.py
@@ -199,18 +199,21 @@ def scale_value(value: Union[int, float],
     return value
 
 
+@func.disallow_variable_format
 def get_lowest_value(clip: vs.VideoNode, chroma: bool = False) -> float:
     is_float = clip.format.sample_type == vs.FLOAT
 
     return -0.5 if chroma and is_float else 0
 
 
+@func.disallow_variable_format
 def get_neutral_value(clip: vs.VideoNode, chroma: bool = False) -> float:
     is_float = clip.format.sample_type == vs.FLOAT
 
-    return (0 if chroma else 0.5) if is_float else get_peak_value(clip) // 2
+    return (0 if chroma else 0.5) if is_float else (1 << get_depth(clip) - 1)
 
 
+@func.disallow_variable_format
 def get_peak_value(clip: vs.VideoNode, chroma: bool = False) -> float:
     is_float = clip.format.sample_type == vs.FLOAT
 
